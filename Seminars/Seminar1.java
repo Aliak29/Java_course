9c5a126134398a5837643e22311c8ef7960e67e8
Aliya <karimova_aly@mail.ru>
2023-03-12 15:18:19 +0500

    Homework1

--- Seminars/Seminar1.java
+++ Seminars/Seminar1.java
@@ -1,7 +1,27 @@
 package Seminars;
 
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
 public class Seminar1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // System.out.println("Hello world!");
+        // System.out.println(LocalDateTime.now());
+        // Scanner scanner = new Scanner(System.in);
+        // int value_a = scanner.nextInt();
+        // System.out.printf("Inputed value: %d\n", value_a);
+        // scanner.close();
+
+        // Написать программу, которая запросит пользователя ввести
+        // <Имя> в консоли. Получит введенную строку и выведет в консоль
+        // сообщение “Привет, <Имя>!”
+        // Scanner iScanner = new Scanner(System.in);
+        // System.out.printf("Введите имя: ");
+        // String name = iScanner.nextLine();
+        // System.out.printf("Привет, %s!", name);
+        // iScanner.close();
+
+        }
+
     }
 }
